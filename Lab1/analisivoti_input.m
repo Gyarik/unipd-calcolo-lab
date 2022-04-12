3345ccb4efc2ebb426edc81599de04bc22bfae83
Gyarik <alex64.AT93@gmail.com>
2022-04-12 17:35:48 +0200

    comment Lab1

--- Lab1/analisivoti_input.m
+++ Lab1/analisivoti_input.m
@@ -1,14 +1,18 @@
+% Pulisce il Workspace da qualsiasi variabile precedentemente creata
 clear all;
 
+% Dichiarazione dei vettori riga tramite input da utente
 voti = input("Inserisci i voti: ");
 crediti = input("Inserisci i crediti: ");
 
+% Calcoli
 mediana = median(voti);
 media = mean(voti);
 mediap = sum(voti .* crediti)/sum(crediti);
 massimo = max(voti);
 minimo = min(voti);
 
+% Stampa in command window dei risultati
 sprintf("La mediana dei voti e' %d/n", mediana)
 sprintf("La media dei voti e' %d\n", media)
 sprintf("La media ponderata dei voti e' %d\n", mediap)
